92e322fc401669cf737513177fe312a7bed34225
spigc-coder <168054830+spigcoder@users.noreply.github.com>
2025-12-05 14:40:16 +0800

    fdsafsda
    fdsafsdfsa

--- argo/pkg/errs/error_test.go
+++ argo/pkg/errs/error_test.go
@@ -1,9 +1,7 @@
 package errs
 
 import (
-	"encoding/json"
 	"errors"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -51,37 +49,6 @@ func TestError_Body(t *testing.T) {
 		"message":     "invalid message",
 		"description": "500 - unknown: invalid message, invalid error",
 	}, body)
-}
-
-// HTTP 响应
-func TestError_Response(t *testing.T) {
-	// 先模拟一个 gin.Context()
-	gin.SetMode(gin.ReleaseMode)
-	r := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(r)
-	Wrap(OK, "I'm fine").WithMeta(Metadata{
-		"foo": "bar",
-	}).Response(ctx)
-	// 查看 ctx 响应
-	assert.Equal(t, OK.StatusCode(), r.Code)
-	// 对请求体进行解码
-	var data gin.H
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if !assert.NoError(t, err) {
-		t.FailNow()
-	}
-	assert.Equal(t, gin.H{
-		// 这里转成 float64 是因为 Go 的 JSON 包会将数字默认解析为 float64
-		"status":      float64(OK.StatusCode()),
-		"code":        OK.String(),
-		"message":     "I'm fine",
-		"description": "200 - ok: I'm fine",
-	}, data)
-	// 通过 ctx.Get 获取传递的错误对象
-	e, ok := ctx.Get("errs")
-	assert.True(t, ok)
-	d, _ := e.(Error)
-	assert.Equal(t, OK, d.Code)
 }
 
 // 返回错误的文本信息
